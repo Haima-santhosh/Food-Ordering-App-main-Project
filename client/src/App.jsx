34a23b2343413e2bee2110f8a6476f94ba945cf8
Haima-santhosh <hymasanthosh14@gmail.com>
2025-11-03 00:12:17 +0530

    Initial React and tailwind setup completed

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      
+     <div>
+      <h1 className='text-4xl font-extrabold text-red-800'>Hello World!</h1>
+
+     </div>
+      </>
+      
+  )
+}
+
+export default App
