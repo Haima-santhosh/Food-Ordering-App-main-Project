326d496f78eabc6a35fe7ce5d9a819eeedf008df
Haima-santhosh <hymasanthosh14@gmail.com>
2025-11-26 01:58:38 +0530

    Updated frontend/backend fixes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,9 @@
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // https://food-ordering-app-main-project.onrender.com/api
-  withCredentials: true, // important for cookies/session
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 export default api;
