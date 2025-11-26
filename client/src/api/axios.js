f4136d36d08a78570fd6794879064d2851590c73
Haima-santhosh <hymasanthosh14@gmail.com>
2025-11-26 21:15:30 +0530

    Updated frontend/backend fixes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,12 +1,11 @@
-// src/api/axios.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://food-ordering-app-main-project-server.onrender.com/api", // Backend URL
+  baseURL: import.meta.env.VITE_API_URL,  // dynamic from env
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, // Enable if using cookies/session auth
+  withCredentials: true,
 });
 
 export default api;
